48495166ac4e16c7288b8bc3b3238aefe150caff
kdlanguido <kdlanguido@gmail.com>
2026-03-12 13:46:19 +0800

    added vault

--- README.md
+++ README.md
@@ -2,4 +2,13 @@
 ```bash
 $ pnpm i
 $ npx prisma generate
+
+$ npx dotenv-vault@latest login
+$ npx dotenv-vault@latest open
+$ npx dotenv-vault@latest pull
+
+
+
+
+
 ```
--- src/config/jwt.config.ts
+++ src/config/jwt.config.ts
@@ -1,23 +1,6 @@
 import {registerAs } from "@nestjs/config"
 import { JwtModuleOptions } from "@nestjs/jwt";
 
-// export const jwtConfig = (configService: ConfigService) => {
-
-//     const secret = configService.get<string>('JWT_SECRET');
-//     const expiresIn = configService.get<string>('JWT_EXPIRES_IN');
-
-// if (!secret || !expiresIn) {
-//     throw new Error('JWTSECRET or JWTEXPIRES not found!')
-// }
-
-// return {
-//     secret,
-//     signOptions: {
-//         expiresIn: expiresIn as any
-//     }
-// }
-// }
-
 export default registerAs('jwt', (): JwtModuleOptions => ({
     secret: process.env.JWT_SECRET! as string,
     signOptions: {
