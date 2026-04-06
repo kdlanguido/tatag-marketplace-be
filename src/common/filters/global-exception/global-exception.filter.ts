05f447b64af2634a6c3731886ec00f25eea7a087
kdlanguido <kdlanguido@gmail.com>
2026-04-06 16:42:15 +0800

    added req url in global exception filter

--- src/common/filters/global-exception/global-exception.filter.ts
+++ src/common/filters/global-exception/global-exception.filter.ts
@@ -14,7 +14,7 @@ export class GlobalExceptionFilter implements ExceptionFilter {
     const request = ctx.getRequest();
 
     let status = HttpStatus.INTERNAL_SERVER_ERROR;
-    let message = 'Internal server error';
+    let message = request.url + ': Internal server error';
 
     if (exception instanceof HttpException) {
       status = exception.getStatus();
